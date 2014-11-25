82d12db8140e3deb2abaf45ac3725d028775cd6c
codewalker <codewalker@hardkernel.com>
2014-11-25 13:58:39 +0900

    ODROID: Fix build error.
    Change-Id: Ic153bd550930cc4d450420d6332e9c1f7348d645

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -70,6 +70,7 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
