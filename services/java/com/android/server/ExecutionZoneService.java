f1a902d21f7b1149ee52866580d227cb591505e2
Md Shahrear Iqbal <shahrear.iqbal@gmail.com>
2015-10-13 18:23:49 -0400

    work on progress, end of office work oct 13

--- services/java/com/android/server/ExecutionZoneService.java
+++ services/java/com/android/server/ExecutionZoneService.java
@@ -1,6 +1,9 @@
 package com.android.server;
 
 import android.content.ContentValues;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,8 +15,9 @@ import android.os.Bundle;
 import android.content.Context;
 import android.os.Message;
 import android.util.Log;
-
 import java.io.File;
+import java.util.List;
+
 
 
 /**
@@ -33,6 +37,16 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
     private static final String APPZONES_ID = "_id";
     private static final String APPZONES_APP_NAME = "app_name";
     private static final String APPZONES_ZONE_ID = "zone_id";
+
+    private static final String TABLE_POLICIES = "policies";
+    private static final String POLICIES_ID = "_id";
+    private static final String POLICIES_NAME = "policy_name";
+    private static final String POLICIES_RULES = "policy_rules";
+
+    private static final String TABLE_ZONEPOLICIES = "zonepolicies";
+    private static final String ZONEPOLICIES_ID = "_id";
+    private static final String ZONEPOLICIES_ZONE_ID = "zone_id";
+    private static final String ZONEPOLICIES_POLICYLIST = "policy_list";
 
     private final Object zonedbLock = new Object();
     private final DatabaseHelper openHelper;
@@ -60,7 +74,6 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
         Bundle b = new Bundle();
 
         // Storing data into bundle
-        b.putString("what", "SET_ZONE");
         b.putString("packagename", packageName);
         b.putString("zonename", zoneName);
 
@@ -106,6 +119,31 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
         return (int) zone_id_long;
     }
 
+    private int getPolicyID (String policyName)
+    {
+        long zone_id_long = 0;
+
+        synchronized (zonedbLock) {
+            final SQLiteDatabase db = openHelper.getReadableDatabase();
+            db.beginTransaction();
+            try {
+                zone_id_long = DatabaseUtils.longForQuery(db,
+                        "select "+ POLICIES_ID +" from " + TABLE_POLICIES
+                                + " WHERE " + POLICIES_NAME + "=?",
+                        new String[]{policyName});
+
+                db.setTransactionSuccessful();
+            } catch (Exception e) {
+                // Log, don't crash!
+                Log.e(TAG, "Exception in getPolicyID");
+            } finally {
+                db.endTransaction();
+            }
+        }
+
+        return (int) zone_id_long;
+    }
+
     private boolean setZoneToApp (String packageName, String zoneName)
     {
         synchronized (zonedbLock) {
@@ -136,7 +174,12 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
     }
 
     private class ExecutionZoneWorkerHandler extends Handler {
-        private static final int SET_ZONE = 0;
+        private static final int CREATE_ZONE = 100;
+        private static final int SET_ZONE = 101;
+        private static final int EDIT_ZONE = 102;
+        private static final int CREATE_POLICY = 200;
+        private static final int SET_POLICY = 201;
+        private static final int EDIT_POLICY = 202;
         @Override
         public void handleMessage(Message msg) {
             try {
@@ -144,6 +187,26 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
                     Log.i(TAG,"set zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
                     if(setZoneToApp(msg.getData().getString("packagename"),msg.getData().getString("zonename")))
                         Log.i(TAG, "zone info inserted successfully.");
+                }
+                if (msg.what == CREATE_ZONE) {
+                    Log.i(TAG,"create zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
+
+                }
+                if (msg.what == EDIT_ZONE) {
+                    Log.i(TAG,"create zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
+
+                }
+                if (msg.what == CREATE_POLICY) {
+                    Log.i(TAG,"create zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
+
+                }
+                if (msg.what == EDIT_POLICY) {
+                    Log.i(TAG,"create zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
+
+                }
+                if (msg.what == SET_POLICY) {
+                    Log.i(TAG,"create zone message received:" + msg.getData().getString("packagename") + " assign to zone " + msg.getData().getString("zonename"));
+
                 }
             } catch (Exception e) {
                 // Log, don't crash!
@@ -160,10 +223,62 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
         return databaseFile.getPath();
     }
 
+    private static String getPolicyString(String policyName) {
+
+        String policyString = "";
+
+        if(policyName.equals("NEW"))
+            policyString = getDefaultAllDangerousDenyPolicyString();
+        else if(policyName.equals("TRUSTED"))
+            policyString = getDefaultAllDangerousDenyPolicyString();
+        else if(policyName.equals("UNTRUSTED"))
+            policyString = getDefaultAllDangerousDenyPolicyString();
+        else if(policyName.equals("RESTRICTED"))
+            policyString = getDefaultAllDangerousDenyPolicyString();
+        else
+        {
+            //go to database and return
+        }
+
+        return policyString;
+    }
+
+    private static String getDefaultAllDangerousDenyPolicyString() {
+
+        return "<DEFAULT,READ_CALENDAR,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,WRITE_CALENDAR,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,CAMERA,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,READ_CONTACTS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,WRITE_CONTACTS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,GET_ACCOUNTS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,ACCESS_FINE_LOCATION,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,ACCESS_COARSE_LOCATION,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,RECORD_AUDIO,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,READ_PHONE_STATE,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,CALL_PHONE,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,READ_CALL_LOG,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,WRITE_CALL_LOG,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,ADD_VOICEMAIL,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,USE_SIP,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,PROCESS_OUTGOING_CALLS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,BODY_SENSORS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,SEND_SMS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,RECEIVE_SMS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,READ_SMS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,RECEIVE_WAP_PUSH,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,RECEIVE_MMS,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,READ_EXTERNAL_STORAGE,TIME_ALWAYS,DENY>;" +
+                "<DEFAULT,WRITE_EXTERNAL_STORAGE,TIME_ALWAYS,DENY>";
+    }
+
     static class DatabaseHelper extends SQLiteOpenHelper {
+
+        Context dbhContext;
 
         public DatabaseHelper(Context context) {
             super(context, ExecutionZoneService.getDatabaseName(), null, DATABASE_VERSION);
+
+            dbhContext = context;
         }
 
         /**
@@ -173,41 +288,167 @@ public class ExecutionZoneService extends IExecutionZoneService.Stub {
          */
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE " + TABLE_ZONES + " ( "
-                    + ZONES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + ZONES_NAME + " TEXT NOT NULL, "
-                    + "UNIQUE(" + ZONES_NAME + "))");
 
-            db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
-                    + ZONES_NAME + ") "
-                    + " VALUES ( "
-                    + "'NEW' );");
+            try {
+                //create table for zones
+                db.execSQL("CREATE TABLE " + TABLE_ZONES + " ( "
+                        + ZONES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                        + ZONES_NAME + " TEXT NOT NULL, "
+                        + "UNIQUE(" + ZONES_NAME + "))");
 
-            db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
-                    + ZONES_NAME + ") "
-                    + " VALUES ( "
-                    + "'TRUSTED' );");
+                //add default zones shah shah
 
-            db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
-                    + ZONES_NAME + ") "
-                    + " VALUES ( "
-                    + "'UNTRUSTED' );");
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
+                        + ZONES_NAME + ") "
+                        + " VALUES ( "
+                        + "'NEW' );");
 
-            db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
-                    + ZONES_NAME + ") "
-                    + " VALUES ( "
-                    + "'RESTRICTED' );");
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
+                        + ZONES_NAME + ") "
+                        + " VALUES ( "
+                        + "'TRUSTED' );");
 
-            db.execSQL("CREATE TABLE " + TABLE_APPZONES + " (  "
-                    + APPZONES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,  "
-                    + APPZONES_ZONE_ID + " INTEGER NOT NULL, "
-                    + APPZONES_APP_NAME + " TEXT NOT NULL,  "
-                    + "UNIQUE (" + APPZONES_APP_NAME + "))");
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
+                        + ZONES_NAME + ") "
+                        + " VALUES ( "
+                        + "'UNTRUSTED' );");
 
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONES + " ( "
+                        + ZONES_NAME + ") "
+                        + " VALUES ( "
+                        + "'RESTRICTED' );");
 
-            //ADD FOR LOOP TO ADD ALL SYSTEM APPS TO TRUSTED ZONE SHAH SHAH OCT 6
+                //create table to save zone to app mapping
+
+                db.execSQL("CREATE TABLE " + TABLE_APPZONES + " (  "
+                        + APPZONES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,  "
+                        + APPZONES_ZONE_ID + " INTEGER NOT NULL, "
+                        + APPZONES_APP_NAME + " TEXT NOT NULL,  "
+                        + "UNIQUE (" + APPZONES_APP_NAME + "))");
+
+                //ADD FOR LOOP TO ADD ALL SYSTEM APPS TO TRUSTED ZONE SHAH SHAH OCT 6
+
+                PackageManager packageManager = dbhContext.getPackageManager();
+                List<ApplicationInfo> list = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+                int zoneID = 0;
+
+                Cursor c = db.rawQuery("select " + ZONES_ID + " from " + TABLE_ZONES
+                                + " WHERE " + ZONES_NAME + "=?",
+                        new String[]{"TRUSTED"});
+
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+
+                    zoneID = c.getInt(0);
+                }
+
+                for (ApplicationInfo info : list) {
+
+                    db.execSQL("INSERT OR REPLACE INTO " + TABLE_APPZONES + " (  "
+                            + APPZONES_ZONE_ID + ", "
+                            + APPZONES_APP_NAME + ") "
+                            + " VALUES ( "
+                            + zoneID
+                            + ", '" + info.packageName + "'"
+                            + ");");
+                }
+
+                //create table for policy
+                db.execSQL("CREATE TABLE " + TABLE_POLICIES + " (  "
+                        + POLICIES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,  "
+                        + POLICIES_NAME + " TEXT NOT NULL, "
+                        + POLICIES_RULES + " TEXT NOT NULL,  "
+                        + "UNIQUE (" + POLICIES_NAME + "))");
+
+                //add default policies
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_POLICIES + " (  "
+                        + POLICIES_NAME + ", "
+                        + POLICIES_RULES + ") "
+                        + " VALUES ( "
+                        + "'DENY_ALL_DANGEROUS'"
+                        + ", '" + getDefaultAllDangerousDenyPolicyString() + "'"
+                        + ");");
+
+                //create table for zone to policy mapping
+                db.execSQL("CREATE TABLE " + TABLE_ZONEPOLICIES + " (  "
+                        + ZONEPOLICIES_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,  "
+                        + ZONEPOLICIES_ZONE_ID + " INTEGER NOT NULL, "
+                        + ZONEPOLICIES_POLICYLIST + " TEXT NOT NULL,  "
+                        + "UNIQUE (" + APPZONES_APP_NAME + "))");
+
+                //add default zone policy mappings
+
+                c = db.rawQuery("select " + ZONES_ID + " from " + TABLE_ZONES
+                                + " WHERE " + ZONES_NAME + "=?",
+                        new String[]{"NEW"});
+
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+
+                    zoneID = c.getInt(0);
+                }
+
+                int policyID = 0;
+
+                c = db.rawQuery("select " + POLICIES_ID + " from " + TABLE_POLICIES
+                                + " WHERE " + POLICIES_NAME + "=?",
+                        new String[]{"DENY_ALL_DANGEROUS"});
+
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+
+                    policyID = c.getInt(0);
+                }
+
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONEPOLICIES + " (  "
+                        + ZONEPOLICIES_ZONE_ID + ", "
+                        + ZONEPOLICIES_POLICYLIST + ") "
+                        + " VALUES ( "
+                        + zoneID
+                        + ", '" + policyID + "'"
+                        + ");");
+
+                c = db.rawQuery("select " + ZONES_ID + " from " + TABLE_ZONES
+                                + " WHERE " + ZONES_NAME + "=?",
+                        new String[]{"UNTRUSTED"});
+
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+
+                    zoneID = c.getInt(0);
+                }
+
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONEPOLICIES + " (  "
+                        + ZONEPOLICIES_ZONE_ID + ", "
+                        + ZONEPOLICIES_POLICYLIST + ") "
+                        + " VALUES ( "
+                        + zoneID
+                        + ", '" + policyID + "'"
+                        + ");");
+
+                c = db.rawQuery("select " + ZONES_ID + " from " + TABLE_ZONES
+                                + " WHERE " + ZONES_NAME + "=?",
+                        new String[]{"RESTRICTED"});
+
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+
+                    zoneID = c.getInt(0);
+                }
+                db.execSQL("INSERT OR REPLACE INTO " + TABLE_ZONEPOLICIES + " (  "
+                        + ZONEPOLICIES_ZONE_ID + ", "
+                        + ZONEPOLICIES_POLICYLIST + ") "
+                        + " VALUES ( "
+                        + zoneID
+                        + ", '" + policyID + "'"
+                        + ");");
+            }
+            catch (Exception once)
+            {
+                Log.e(TAG, "Exception occurred while creating zones.db. The exception message is; "+ once.getMessage());
+            }
         }
-
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             Log.e(TAG, "upgrade from version " + oldVersion + " to version " + newVersion);
